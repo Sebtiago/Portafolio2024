4b0b08d26f4406fb7975e2605bc06306915175b7
Sebastián Suárez <santiago.sua77@gmail.com>
2024-02-05 20:58:06 -0500

    Update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel/serverless";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: vercel()
 });
-
-export default {
+export default defineConfig({
   // ...
   build: {
     rollupOptions: {
-      external: ['sharp'], // Agrega 'sharp' como dependencia externa
-    },
-  },
+      external: ['sharp'] // Agrega 'sharp' como dependencia externa
+    }
+  }
   // ...
-};
+  ,
+  integrations: [tailwind()]
+});
